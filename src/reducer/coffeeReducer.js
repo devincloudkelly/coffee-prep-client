7cb17951bd19e2b1c08c268e0819bba6eea3adfa
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-29 15:45:43 -0800

    built basic reducer

--- src/reducer/coffeeReducer.js
+++ src/reducer/coffeeReducer.js
@@ -3,5 +3,15 @@
 const initialState = {key: 'initial state'}
 
 const state = {
-    
+    //just have placeholders in state right now. Need to remove once setup.
+    key: value
+}
+
+export const reducer = (state={initialState}, action) => {
+    switch (action.type) {
+        case 'EXAMPLE':
+            return state
+        default:
+            return state
+    }
 }
