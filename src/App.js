162365a901cd4d8c4ac82c03772165e8e7004bfe
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-01-30 09:57:45 -0800

    added routing for main screens

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ import ShowPreparation from './containers/ShowPreparation';
 function App() {
   return (
     <div >
-      {/* <Router>
-        
-      </Router> */}
-      <HomeScreen/>
+      <Router>
+        <Route exact path='/'>
+          <HomeScreen/>
+        </Route>
+        <Route path='/profile'>
       <Profile/>
+        </Route>
+        <Route exact path='/preparation/new'>
       <NewPreparation/>
+        </Route>
+        <Route path='/preparation'>
       <ShowPreparation/>
+        </Route>
+      </Router>
       
       This is the App
     </div>
