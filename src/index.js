603c35e1561f9a204fb60a4492f7f5a460c90f08
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-02-01 12:28:42 -0800

    add state persistence to localStorage

--- src/index.js
+++ src/index.js
@@ -5,8 +5,21 @@ import { Provider } from 'react-redux'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducer/coffeeReducer'
+import store from './services/store'
 
-const store = createStore(reducer)
+
+// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+// function saveToLocalStorage(state) {
+//     try {
+//         const serializedState = JSON.stringify(state)
+//         localStorage.setItem('state', serializedState)
+//     } catch(e) {
+//         console.log(e)
+//     }
+// }
+
+// store.subscribe(() => saveToLocalStorage(store.getState()))
 
 ReactDOM.render(
 <Provider store={store}>
