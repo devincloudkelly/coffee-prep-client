9c0bc25b0ad9741b23b797cd0f8dc36df812ff9c
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-02-03 09:25:05 -0800

    made prep cards clickable

--- src/components/PrepCard.js
+++ src/components/PrepCard.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import {Redirect} from 'react-router-dom'
 
 export class PrepCard extends Component {
+
+    handleClick = (id) => {
+        console.log('handling click on prep card. Id being passed through...', id)
+        const prepUrl = `/preparation/${id}`
+        return <Redirect to={prepUrl} />
+    }
+
     render() {
         console.log(this.props.prep)
         const { id, device, coffee_brand, coffee_name, notes } = this.props.prep
         return (
-            <div>
-                <h3>Place Brew Device icon here</h3>
+            <div onClick={() => this.handleClick(id)}>
+                <h3>Place {device} Device icon here</h3>
                 <h5>{coffee_brand} - {coffee_name}</h5>
                 <p>{notes}</p>
             </div>
