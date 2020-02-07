5fce7cfdab9ba7713ded195aa649082dd2562193
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-02-06 17:17:24 -0800

    fixed controlled action menu for steps form

--- src/components/StepForm.js
+++ src/components/StepForm.js
@@ -4,6 +4,8 @@ import Adapter from '../services/Adapter'
 import CoffeeAction from '../action/coffeeAction';
 import { Form, Container, Segment, Select, Button} from 'semantic-ui-react'
 
+
+
 export class StepForm extends Component {
     state = {
         action: '',
@@ -12,10 +14,25 @@ export class StepForm extends Component {
         order: null,
         directions: ''
     }
+    
+    stepOptions = [
+        { key: 'bloom', text: 'Bloom', value: 'bloom'},
+        { key: 'pour', text: 'Pour', value: 'pour'},
+        { key: 'stir', text: 'Stir', value: 'stir'},
+        { key: 'wait', text: 'Wait', value: 'wait'},
+        { key: 'press', text: 'Press', value: 'press'}
+    ]
 
     handleInput = (e) => {
+        console.log(e.target.name, e.target.value)
         return this.setState({
             [e.target.name]: e.target.value
+        }, ()=>console.log(this.state))
+    }
+    handleSelectInput = (e, { value }) => {
+        console.log(e.target.name, e.target.value, value)
+        return this.setState({
+            action: value
         }, ()=>console.log(this.state))
     }
 
@@ -31,40 +48,47 @@ export class StepForm extends Component {
     }
 
     render() {
+        const { value } = this.state
         return (
             <Segment>
                 <Form onSubmit={this.handleSubmit}>
-                    <Form.Field>
 
-                    <label>
-                        <h3>Prep Action: </h3>
-                    </label>
-                        <select name='action' value={this.state.action} onChange={e => this.handleInput(e)}>
-                            <option value=''>Choose Action</option>
-                            <option value='bloom'>Bloom</option>
-                            <option value='pour'>Pour</option>
-                            <option value='stir'>Stir</option>
-                            <option value='wait'>Wait</option>
-                            <option value='press'>Press</option>
-                        </select>
-                    </Form.Field>
+
                         <h3>Step Specs: </h3>
-                        <Form.Group widths='equal'>
+                    <Form.Group widths='equal'>
+                    <Form.Select 
+                        label='Action'
+                        options={this.stepOptions}
+                        placeholder='Action'
+                        onChange={this.handleSelectInput}
+                    // <label>
+                    //     <h3>Action</h3>
+                    // </label>
+                        // <select name='action' value={this.state.action} >
+                        //     <option value=''>Choose Action</option>
+                        //     <option value='bloom'>Bloom</option>
+                        //     <option value='pour'>Pour</option>
+                        //     <option value='stir'>Stir</option>
+                        //     <option value='wait'>Wait</option>
+                        //     <option value='press'>Press</option>
+                        // </select>
+                        />
+                
                         <Form.Field>
                             <label>Duration (seconds)
                             </label>
-                            <input type='number' name='duration' value={this.state.duration} onChange={e => this.handleInput(e)} placeholder='Duration in seconds'/>
+                            <input type='number' name='duration' value={this.state.duration} onChange={this.handleInput} placeholder='Duration in seconds'/>
                         </Form.Field>
                         <Form.Field>
                             <label>Amount of Water (ml)
                             </label>
-                            <input type='number' name='amount' value={this.state.amount} onChange={e => this.handleInput(e)} placeholder='Amount in ml'/>
+                            <input type='number' name='amount' value={this.state.amount} onChange={this.handleInput} placeholder='Amount in ml'/>
                         </Form.Field>
                         </Form.Group>
                     <h3>Step notes:</h3>
                     <Form.Field>
                         <label>
-                        <textarea type='text' name='directions' value={this.state.directions} onChange={e => this.handleInput(e)} placeholder='Add directions for this step ex. Pour coffee over grounds and stir.'/>
+                        <textarea type='text' name='directions' value={this.state.directions} onChange={this.handleInput} placeholder='Add directions for this step ex. Pour coffee over grounds and stir.'/>
                         </label>
                     </Form.Field>
                     <Form.Field control={Button}>Add Step</Form.Field>
