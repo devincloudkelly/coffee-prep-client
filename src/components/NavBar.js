010eed34de686c4a600442794eb89c548fd31fac
devincloudkelly <devin.cloud.kelly@gmail.com>
2020-02-12 14:53:35 -0800

    added navbar to homescreen and re-ordered login page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,7 +26,19 @@ export class NavBar extends Component {
 
     render() {
         if (!this.props.loggedIn){
-            return <div></div>
+            return <div>
+                <Menu pointing secondary stackable inverted size='large' style={{backgroundColor: '#e34b2a', alignItems: 'center'}}>
+                <Link to='/profile'>
+                    <Menu.Item
+                        // name='profile'
+                        // active={activeItem === 'logo'}
+                        // onClick={this.handleItemClick}
+                        >
+                        <img src={require('/Users/devinkelly/Flatiron/Mod_5/coffee-mentor-project/coffee-prep-client/src/images/coffee-mentor-press-red-logo-white-text.gif')} style={{width: '13rem'}}/>
+                    </Menu.Item>
+                        </Link>
+                        </Menu>
+            </div>
         }
             const { activeItem } = this.state
             return (
--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -24,6 +24,12 @@ class HomeScreen extends Component {
             <Segment inverted>            
             <Grid stackable columns={2} >
                 {/* <Image src='https://images.pexels.com/photos/1235717/pexels-photo-1235717.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'/> */}
+                <Grid.Column  width={5}>
+                {this.state.logIn
+                ? <Login toggleSignIn={this.toggleSignIn}/>
+                : <Signup toggleSignIn={this.toggleSignIn}/>
+                }
+                </Grid.Column>     
                 <Grid.Column className='home-screen-title-column' textAlign='center' width={11} style={{
                     backgroundImage: `url(${background})`,
                     backgroundPosition: 'center',
@@ -34,12 +40,6 @@ class HomeScreen extends Component {
                 <br/><br/><br/><br/>
                 <h2 className='home-screen-snippet'>Coffee Mentor allows you to save your favorite coffee preparations, share them with friends, and learn from the best coffee enthusiasts around the world. Learn, share and start brewing your best!</h2>
                 </Grid.Column>
-                <Grid.Column  width={5}>
-                {this.state.logIn
-                ? <Login toggleSignIn={this.toggleSignIn}/>
-                : <Signup toggleSignIn={this.toggleSignIn}/>
-                }
-                </Grid.Column>     
             </Grid>
             </Segment>
             </Container>
